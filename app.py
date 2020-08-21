f4a80f87d40491b4397022b4b2ce9019ef90be96
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-08-21 11:52:34 +0000

    Added database URI to .bashrc, tested the connection to the database

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
+import pymongo
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "videogames"
+COLLECTION_NAME = "game"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+        
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
+
 
 
 @app.route('/')
